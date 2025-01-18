4d6abd41d6ecb4e85aa466e908939e66073f99f7
Philipp Bönsch <philipp.boensch@softgames.de>
2025-01-18 18:49:04 +0100

    update landing page text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,19 +14,19 @@ const Home: FC = () => {
           <hr />
           <img src="Headkino_logo.png" alt="Headkino Logo" className="logo-image" />
           <p>
-            2 friends making game about ordinary people in a slightly extraordinary
+            Friends making game about ordinary people in a slightly extraordinary
             situation.
           </p>
           <div className="social-icons">
             <a href="https://headkino.itch.io/" target="_blank" rel="noopener noreferrer">
               <img src="itchio-black.svg" alt="Itch.io" className="social-icon itch" />
             </a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://instagram.com/headkino.games" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-instagram social-icon"></i>
             </a>
-            <a href="https://threads.net" target="_blank" rel="noopener noreferrer">
+            {/* <a href="https://threads.net/@headkino.games" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-threads social-icon"></i>
-            </a>
+            </a> */}
             <a
               href="https://bsky.app/profile/headkino.bsky.social"
               target="_blank"
@@ -41,18 +41,24 @@ const Home: FC = () => {
             >
               <i className="fab fa-youtube social-icon"></i>
             </a>
-            <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://tiktok.com/@headkino.games" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-tiktok social-icon"></i>
             </a>
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            {/* <a href="https://www.linkedin.com/company/headkino" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-linkedin social-icon"></i>
-            </a>
+            </a> */}
             <a
               href="mailto:headkinogames@gmail.com"
               target="_blank"
               rel="noopener noreferrer"
             >
               <i className="fas fa-envelope social-icon"></i>
+            </a>
+            <a href="https://reddit.com/r/headkino" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-reddit social-icon"></i>
+            </a>
+            <a href="https://discord.gg/headkino" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-discord social-icon"></i>
             </a>
           </div>
         </div>
