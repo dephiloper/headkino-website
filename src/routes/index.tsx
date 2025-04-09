9c10e07b364e024a1dd701a998d11ea81c9a1394
boesh <philipp.boensch@pm.me>
2025-04-09 17:35:02 +0200

    using hashrouter for static gh page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,21 @@
-import React, { FC } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { FC } from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
-import NotFound from '../pages/NotFound'
-import HomeNew from '../pages/Home/index_new'
-import Home from '../pages/Home'
+import NotFound from '../pages/NotFound';
+import HomeNew from '../pages/Home/index_new';
+import Home from '../pages/Home';
 
 const RouteStack: FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<HomeNew />} />
-
         {/* not found routes will hit here */}
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
-  )
-}
+    </HashRouter>
+  );
+};
 
-export default RouteStack
+export default RouteStack;
